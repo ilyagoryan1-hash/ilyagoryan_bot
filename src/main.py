bab3be716c0d08dd84332e0114e494ac3ccc048c
ilyagoryan1-hash <ilyagoryan1@gmail.com>
2025-11-08 20:52:16 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -236,4 +236,7 @@ async def mark_watched(callback: CallbackQuery):
 if __name__ == "__main__":
     import asyncio
     asyncio.run(init_db())
+    
+    import asyncio
+asyncio.set_event_loop(asyncio.new_event_loop())
     executor.start_polling(dp, skip_updates=True)
